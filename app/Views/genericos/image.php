d1b99b970047ed2cfcf16a3bb72957d8ff967cf5
CristopherMartinez <martinezcristopher69@gmail.com>
2023-02-07 18:37:39 -0600

    Prueba

--- app/Views/genericos/image.php
+++ app/Views/genericos/image.php
@@ -1,2 +1,5 @@
 <!-- Image of location/map -->
 <img src="<?php  echo base_url()?>/imagenes/map.jpg" class="w3-image w3-greyscale-min" style="width:100%">
+
+<!--Prueba-->
+<div>Esto es una prueba</div>
